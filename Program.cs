80b9c055638154c3ba69ad8bed7903575e6b939a
Yana <vip.yana.baranova.2000@mail.ru>
2023-06-10 16:32:00 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿﻿Console.WriteLine("Enter first number:");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number:");
+int secondNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter third number:");
+int thirdNumber = int.Parse(Console.ReadLine());
+
+int max = firstNumber;
+if (secondNumber>max) max = secondNumber;
+if (thirdNumber>max) max = thirdNumber;
+
+Console.WriteLine($"max={max}");
